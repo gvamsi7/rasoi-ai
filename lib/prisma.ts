532001a9320786bd3dac332a8094ce7f072fa9a0
vamshi <gvamshi906@gmail.com>
2026-09-06 19:14:40 +0530

    fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,12 @@
-import {PrismaClient} from '@prisma/client';
-declare global { var prisma: PrismaClient | undefined }
-export const prisma=global.prisma||new PrismaClient();
-if(process.env.NODE_ENV!=='production')global.prisma=prisma;
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
